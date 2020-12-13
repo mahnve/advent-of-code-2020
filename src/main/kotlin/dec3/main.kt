e9f9c356e2a2473759ac8346e1389d81730c197e
Marcus Ahnve <marcus@ahnve.com>
2020-12-13 23:00:21 +0100

    dec3 part 2

--- src/main/kotlin/dec3/main.kt
+++ src/main/kotlin/dec3/main.kt
@@ -3,15 +3,30 @@ package dec3
 var data = {}.javaClass.getResource("input.txt").openStream().bufferedReader().readLines()
 
 fun main() {
-    val result = data.fold(Pair(0, 0)) { posAndTotal, line ->
-        if (hasTree(line, posAndTotal.first))
-            Pair(posAndTotal.first + 3, posAndTotal.second + 1)
-        else
-            Pair(posAndTotal.first + 3, posAndTotal.second)
-    }
+    val runs = listOf(
+        1 to 1,
+        3 to 1,
+        5 to 1,
+        7 to 1,
+        1 to 2
+    )
 
-    print(result.second)
+    val result = runs.map {countHitTrees(data, it.first, it.second)}.reduce {acc, it -> acc * it}
 
+    print(result)
+
+}
+
+private fun countHitTrees(forest: List<String>, stepsRight: Int, stepsDown: Int): Int {
+    val result = forest
+        .filterIndexed { index, _ -> index % stepsDown == 0 }
+        .fold(Pair(0, 0)) { posAndTotal, line ->
+            if (hasTree(line, posAndTotal.first))
+                Pair(posAndTotal.first + stepsRight, posAndTotal.second + 1)
+            else
+                Pair(posAndTotal.first + stepsRight, posAndTotal.second)
+        }
+    return result.second
 }
 
 fun hasTree(line: String, position: Int): Boolean {
